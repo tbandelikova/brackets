f6cecacaed89fdbf084904da7bc4e9bafb4bd6d3
tat.bandelikova <tat.bandelikova@gmail.com>
2023-02-08 18:36:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 0) {
+    let config = bracketsConfig.slice(0);
+    let input = str.split('');
+    let stack = [];
+
+    input.map(el => toStack(el));
+
+    function toStack(bracket) {
+      if (config.map(item => item[0] == bracket).includes(true)) {
+        if (config.map(item => item[1] == bracket).includes(true)) {
+          checkSame(bracket);
+        } else {
+          stack.push(bracket);
+        }
+      } else {
+        config.find(el => el.includes(bracket))[0] === stack[stack.length - 1] && stack.pop();
+      }
+    }
+
+    function checkSame(bracket) {
+      stack.length == 0 ? stack.push(bracket) : stack[stack.length - 1] === bracket ? stack.pop() : stack.push(bracket);
+    }
+
+    return stack.length == 0;
+  }
+  return false;
 }
